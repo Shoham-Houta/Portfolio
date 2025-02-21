fe7afcc95de06da09693c8e49b1fdb679cedb368
Shoham Houta <shohamho@gmail.com>
2025-02-21 15:30:33 +0200

    Added Contact form TODO: handle the post and the message sending

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from config import config_options
 
 
@@ -6,9 +6,20 @@ app = Flask(__name__)
 app.config.from_object(config_options["development"])
 
 
+@app.context_processor
+def inject_request():
+    return dict(request=request)
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/About")
+def about():
+    pass
+
 
 if __name__ == "__main__":
     app.run(debug=app.config["DEBUG"])
